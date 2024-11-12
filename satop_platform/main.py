a6658a3dd75b4aac049a6152cf6f167671f8821b
Nikolasjen <aleknik00@gmail.com>
2024-11-12 12:41:46 +0100

    removed redundant logging level implementation

--- satop_platform/main.py
+++ satop_platform/main.py
@@ -5,19 +5,6 @@ from components import restapi as api
 from components import sample
 from plugin_engine.plugin_engine import run_engine
 
-# Parse command-line arguments
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('-v', action='count', default=0, help="Increase verbosity level (use -v or -vv)")
-args = parser.parse_args()
-
-# Set the logging level based on the verbosity flag
-if args.v == 1:
-    logging.basicConfig(level=logging.INFO)  # -v (less verbose)
-elif args.v >= 2:
-    logging.basicConfig(level=logging.DEBUG)  # -vv (more verbose)
-else:
-    logging.basicConfig(level=logging.WARNING)  # default logging level
 
 logger = logging.getLogger(__name__)
 
