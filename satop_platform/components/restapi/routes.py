c90e3e83fd77e7571795a899634e33a7c159789b
Tobias Frejo <tobias@frejo.dk>
2024-11-19 08:55:11 +0100

    Redirect root to docs

--- satop_platform/components/restapi/routes.py
+++ satop_platform/components/restapi/routes.py
@@ -1,8 +1,16 @@
 from fastapi import APIRouter
+from fastapi.responses import RedirectResponse
 
 from .restapi import APIApplication
 
 def load_routes(api_app: APIApplication):
+
+    root_router = APIRouter(include_in_schema=False)
+    @root_router.get('/')
+    async def docs_redirect():
+        return RedirectResponse('/docs')
+    
+    api_app.include_router(root_router)
 
     router = APIRouter(prefix=api_app._root_path, tags=['Platform Core'])
 
