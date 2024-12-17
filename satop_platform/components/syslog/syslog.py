6f03e4f10165f468ba477d3fed60e392f0ae0752
Tobias Frejo <tobias@frejo.dk>
2024-12-17 21:07:45 +0100

    artifact upload and event logging to default log

--- satop_platform/components/syslog/syslog.py
+++ satop_platform/components/syslog/syslog.py
@@ -1,52 +1,77 @@
 import logging
 import hashlib
-from uuid import uuid4
-from fastapi import APIRouter, File, UploadFile
+import os
+import shutil
+from typing import Annotated, Optional
+from fastapi import APIRouter, Depends, File, Form, HTTPException, UploadFile, Request, status
+from fastapi.responses import FileResponse
+import sqlalchemy
+import sqlmodel
+import re
 from ..restapi import APIApplication
 from . import models
+
+ARTIFACT_DIR = './artifact_data/'
 
 logger = logging.getLogger(__name__)
 
 
+def log_event(event: models.Event):
+    logger.info(f'Logged event: {event.model_dump_json()}')
+
+def create_artifact(file, filename):
+    pass
+
+
 def init(rest_app: APIApplication):
     logger.info('Setting up system logger')
+    
+    db = sqlmodel.create_engine('sqlite:///artifacts.db')
+    sqlmodel.SQLModel.metadata.create_all(db, [models.ArtifactStore.__table__])
 
     router = APIRouter(
         prefix='/log',
         tags=['Logging']
     )
 
-    @router.post('/events', status_code=201)
-    def log_event(event_triplet: models.Triplet):
-        logger.info(event_triplet)
-        return 'Created'
-    
+    @router.post('/events')
+    async def new_log_event(event: models.Event):
+        log_event(event)
+        return 'OK'
+
     @router.post('/artifacts', status_code=201)
-    def new_artifact(file: UploadFile):
-        logger.debug(f'Received new artefact "{file.filename}" ({file.size})')
-        hash = {
-            'md5': hashlib.md5(file.file.read()).hexdigest(),
-            'sha1': hashlib.sha1(file.file.read()).hexdigest(),
-            'sha256': hashlib.sha256(file.file.read()).hexdigest()
-        }
-        uuid = uuid4()
+    def upload_artifact(file: Optional[UploadFile]):
+        sha1 = hashlib.sha1(file.file.read()).hexdigest()
 
-        # TODO: save artifact
-        # TODO: track in a database
+        file_model = models.ArtifactStore(sha1=sha1, name=file.filename, size=file.size)
+        logger.debug(f'Received new artefact {file_model.model_dump()}')
+        dump = file_model.model_dump_json()
+        
+        try:
+            with sqlmodel.Session(db) as session:
+                session.add(file_model)
+                session.commit()
+        except sqlalchemy.exc.IntegrityError: 
+            raise HTTPException(status_code=status.HTTP_200_OK, detail='Artifact already exists. Reupload not neccessary')
+        
+        if not os.path.exists(ARTIFACT_DIR):
+            os.mkdir(ARTIFACT_DIR)
+        
+        with open(os.path.join(ARTIFACT_DIR, sha1.lower()), 'wb+') as out_file:
+            file.file.seek(0)
+            shutil.copyfileobj(file.file, out_file)
+            out_file.close()
 
-        return {
-            'hash': hash,
-            'uuid': uuid,
-            'ni': f'ni:///sha-256;{hash.get('sha256')}'
-        }
+        return dump
 
+    @router.get('/artifacts/{hash}')
+    def get_artifact(hash: str):
+        file = os.path.join(ARTIFACT_DIR, re.sub(r'[^0-9a-z]', '_', hash.lower()))
+        logger.debug(f'Getting artifact file at "{file}"')
 
-    @router.get('/artifacts/{uuid}')
-    def get_artifact(uuid: str):
-        pass
-    
-    @router.get('/artifacts/ni/{alg}/{hash}')
-    def get_artifact_ni(alg:str, hash:str):
-        pass
+        if not os.path.exists(file):
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Artifact not found')
+
+        return FileResponse(file)
 
     rest_app.include_router(router)
